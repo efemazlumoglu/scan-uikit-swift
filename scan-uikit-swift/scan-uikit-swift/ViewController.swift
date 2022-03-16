c1b4127cc3f597a487f532b50d2a5364a06208af
efemaz <efe.mazlumoglu@gmail.com>
2022-03-16 14:54:25 +0300

    initial

---
+++ scan-uikit-swift/scan-uikit-swift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scan-uikit-swift
+//
+//  Created by Efe Mazlumoğlu on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
