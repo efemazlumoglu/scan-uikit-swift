e9289eeb937bdc36246a8320f1829930d61df63a
efemaz <efe.mazlumoglu@gmail.com>
2022-03-16 15:21:49 +0300

    Share scan and view controller attributes are added

---
+++ scan-uikit-swift/scan-uikit-swift/Controllers/ShareScanViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ShareScanViewController.swift
+//  scan-uikit-swift
+//
+//  Created by Efe Mazlumoğlu on 16.03.2022.
+//
+
+import UIKit
+
+class ShareScanViewController: UIActivityViewController {
+    
+    init(sourceView: UIView, sharedObject: Any) {
+        super.init(activityItems: [sharedObject], applicationActivities: nil)
+        
+        // Set up popover presentation style
+        modalPresentationStyle = .popover
+        popoverPresentationController?.sourceView = sourceView
+        popoverPresentationController?.sourceRect = sourceView.bounds
+        
+        self.excludedActivityTypes = [.markupAsPDF, .openInIBooks, .message, .print,
+                                      .addToReadingList, .saveToCameraRoll, .assignToContact,
+                                      .copyToPasteboard, .postToTencentWeibo, .postToWeibo,
+                                      .postToVimeo, .postToFlickr, .postToTwitter, .postToFacebook]
+    }
+    
+    deinit {
+        // Restart the session in case it was interrupted by the share sheet
+        if let configuration = ViewController.instance?.sceneView.session.configuration,
+            ViewController.instance?.state == .testing {
+            ViewController.instance?.sceneView.session.run(configuration)
+        }
+    }
+}
+
---
+++ scan-uikit-swift/scan-uikit-swift/Controllers/ViewController+Gestures.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController+Gestures.swift
+//  scan-uikit-swift
+//
+//  Created by Efe Mazlumoğlu on 16.03.2022.
+//
+
+import UIKit
+import SceneKit
+
+extension ViewController: UIGestureRecognizerDelegate {
+        
+    @IBAction func didTap(_ gesture: UITapGestureRecognizer) {
+        if state == .scanning {
+            scan?.didTap(gesture)
+        }
+        
+        instructionsVisible = false
+    }
+    
+    @IBAction func didOneFingerPan(_ gesture: UIPanGestureRecognizer) {
+        if state == .scanning {
+            scan?.didOneFingerPan(gesture)
+        }
+        
+        instructionsVisible = false
+    }
+    
+    @IBAction func didTwoFingerPan(_ gesture: ThresholdPanGestureRecognizer) {
+        if state == .scanning {
+            scan?.didTwoFingerPan(gesture)
+        }
+        
+        instructionsVisible = false
+    }
+    
+    @IBAction func didRotate(_ gesture: ThresholdRotationGestureRecognizer) {
+        if state == .scanning {
+            scan?.didRotate(gesture)
+        }
+        
+        instructionsVisible = false
+    }
+    
+    @IBAction func didLongPress(_ gesture: UILongPressGestureRecognizer) {
+        if state == .scanning {
+            scan?.didLongPress(gesture)
+        }
+        
+        instructionsVisible = false
+    }
+    
+    @IBAction func didPinch(_ gesture: ThresholdPinchGestureRecognizer) {
+        if state == .scanning {
+            scan?.didPinch(gesture)
+        }
+        
+        instructionsVisible = false
+    }
+    
+    func gestureRecognizer(_ first: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith second: UIGestureRecognizer) -> Bool {
+        if first is UIRotationGestureRecognizer && second is UIPinchGestureRecognizer {
+            return true
+        } else if first is UIRotationGestureRecognizer && second is UIPanGestureRecognizer {
+            return true
+        } else if first is UIPinchGestureRecognizer && second is UIRotationGestureRecognizer {
+            return true
+        } else if first is UIPinchGestureRecognizer && second is UIPanGestureRecognizer {
+            return true
+        } else if first is UIPanGestureRecognizer && second is UIPinchGestureRecognizer {
+            return true
+        } else if first is UIPanGestureRecognizer && second is UIRotationGestureRecognizer {
+            return true
+        }
+        return false
+    }
+}
