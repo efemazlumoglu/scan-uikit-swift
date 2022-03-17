2053efd5eb1f887c7a3e3eec9848904cb96dc7f7
efemaz <efe.mazlumoglu@gmail.com>
2022-03-17 15:21:23 +0300

    detected bounding box

---
+++ scan-uikit-swift/scan-uikit-swift/3D UI and Visualizations/DetectedBoundingBox.swift
@@ -0,0 +1,34 @@
+//
+//  DetectedBoundingBox.swift
+//  scan-uikit-swift
+//
+//  Created by Efe Mazlumoğlu on 17.03.2022.
+//
+
+import Foundation
+import ARKit
+
+class DetectedBoundingBox: SCNNode {
+    
+    init(points: [float3], scale: CGFloat, color: UIColor = .appYellow) {
+        super.init()
+        
+        var localMin = float3(Float.greatestFiniteMagnitude)
+        var localMax = float3(-Float.greatestFiniteMagnitude)
+        
+        for point in points {
+            localMin = min(localMin, point)
+            localMax = max(localMax, point)
+        }
+        
+        self.simdPosition += (localMax + localMin) / 2
+        let extent = localMax - localMin
+        let wireframe = Wireframe(extent: extent, color: color, scale: scale)
+        self.addChildNode(wireframe)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
